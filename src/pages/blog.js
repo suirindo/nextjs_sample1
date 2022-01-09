55d69f8be0819dc2d415aa1800e0164f65822808
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-09 12:31:14 +0900

    20220109 2nd

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,19 @@
+import Link from 'next/link'
 import matter from "gray-matter"
 
 const Blog = (props) => {
     console.log(props)
     return (
-        <h1> ブログページ </h1>
+        <div>
+            <h1> ブログページ </h1>
+            {props.blogs.map((blog, index) => 
+                <div key={index}>
+                    <h3>{blog.frontmatter.title}</h3>
+                    <p>{blog.frontmatter.date}</p>
+                    <Link href={`/blog/${blog.slug}`}><a>ReadMore</a></Link>
+                </div>
+            )}
+        </div>
     )
 }
 
@@ -13,20 +23,21 @@ export async function getStaticProps() {
   const blogs = ((context) => { 
       const keys = context.keys()
       const values = keys.map(context)
-
       const data = keys.map((key, index) => {
-          let slug = key.replace(/^.*[\\\/]/,").slice(0,-3)
+          let slug = key.replace(/^.*[\\\/]/,'').slice(0,-3)
       const value = values[index]
       const document = matter(value.default)
         return{
             frontmatter: document.data,
             slug: slug
         }
+
       })
       return data
   })(require.context('../data', true, /\.md$/))
     return {
         props: {
+            blogs: JSON.parse(JSON.stringify(blogs))
         },
     }
 }
---
+++ src/pages/blog/[slug].js
@@ -0,0 +1,7 @@
+const SingleBlog = () => {
+    return (
+        <h1>記事ページ</h1>
+    )
+}
+
+export default SinglePage
