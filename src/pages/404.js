cd4d80ddebeb575e51df1359b9a2990efa33dc32
suirindo <lagavulin.jb.dc@gmail.com>
2022-01-11 20:55:28 +0900

    404 modify

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,11 @@ import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-        <Seo title="ページが見つかりません" description="これは404ページです" /> 
-        <div style={{textAlign: "center", height: "70vh"}}>
-            <h1>404: Not Found</h1>
-            <p>ページが見つかりません。</p>
-        </div>
+    <Seo title="ページが見つかりません" description="これは404ページです" /> 
+    <div style={{textAlign: "center", height: "70vh"}}>
+      <h1>404: Not Found</h1>
+      <p>ページが見つかりません。</p>
+    </div>
   </Layout>
 )
 
