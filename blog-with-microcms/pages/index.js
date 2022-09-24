17c846a81469e167876c074794b2a16e08d6654f
asagiman <lagavulin.jb.dc@gmail.com>
2022-09-24 13:44:10 +0900

    0924

--- blog-with-microcms/pages/index.js
+++ blog-with-microcms/pages/index.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { client } from '../libs/client';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 
 // SSGを使ってデータを取得する
 export const getStaticProps = async () => {
